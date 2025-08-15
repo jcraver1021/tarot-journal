2da76ad0f526c9095cb40b9bc9cc02df90e2b7d8
jcraver <25520616+jcraver1021@users.noreply.github.com>
2025-08-14 22:14:53 -0400

    remove home comment

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,18 +2,6 @@ import './Home.css';
 import Banner from '../../components/Banner/Banner';
 
 function Home() {
-  // want:
-  // title typography "Tarot Journal"
-  // centered in the page
-  // with a class of "home"
-  // button to navigate to the tarot journal page
-  // button to navigate to the tarot reading page
-  // button to navigate to the tarot card meanings page
-
-  // TODO: Add buttons to navigate to each page
-  // TODO: Add page to read all journal entries for a profile
-  // TODO: Add saving a journal entry with a profile to local storage
-
   return (
     <div className="home">
       <Banner text="Tarot Journal" />
