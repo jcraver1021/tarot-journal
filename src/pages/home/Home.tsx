9b16e6d7c242733ed1df3b714752bfefd794b621
James Craver <25520616+jcraver1021@users.noreply.github.com>
2025-08-23 19:07:07 -0400

    Adds Home links (#53)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,10 +1,34 @@
-import './Home.css';
+import {Button, Stack} from '@mui/material';
 import Banner from '../../components/Banner/Banner';
+
+import './Home.css';
 
 function Home() {
   return (
     <div className="home">
-      <Banner text="Tarot Journal" />
+      <Stack>
+        <Banner text="Tarot Journal" level={1} />
+        <Stack direction="row" spacing={2}>
+          <Stack>
+            <Banner text="Draw" level={2} />
+            <Button variant="contained" href="/draw/single">
+              Single Card
+            </Button>
+          </Stack>
+          <Stack>
+            <Banner text="Review" level={2} />
+            <Button variant="contained" href="/read">
+              Read your Journal
+            </Button>
+          </Stack>
+          <Stack>
+            <Banner text="Explore" level={2} />
+            <Button variant="contained" href="/card/0">
+              View Cards
+            </Button>
+          </Stack>
+        </Stack>
+      </Stack>
     </div>
   );
 }
