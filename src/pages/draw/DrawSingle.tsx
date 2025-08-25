715065d41c982fe66eb364b07bb696cbc60e7dd7
James Craver <25520616+jcraver1021@users.noreply.github.com>
2025-08-25 18:35:39 -0400

    Adds Instructions component (#57)
    * add note component
    
    * Rename to instructions and add to Draw 1

--- src/pages/draw/DrawSingle.tsx
+++ src/pages/draw/DrawSingle.tsx
@@ -1,5 +1,6 @@
 import {Grid} from '@mui/material';
 import {Card} from '../../hooks/cards/cards';
+import Instructions from '../../components/Instructions/Instructions';
 import TarotCard, {
   DisplayModes,
   Orientation,
@@ -21,6 +22,10 @@ function DrawSingle({card}: DrawSingleProps) {
         alignItems="center"
         justifyContent="center"
       >
+        <Instructions
+          title="Single Card"
+          content="Focus on the meaning of this card. Does it represent today? Or does it represent what you're thinking about most at this moment? Or something else entirely! Write your thoughts in the journal below."
+        />
         <div className="drawCard">
           <TarotCard
             displayMode={DisplayModes.DRAW_SINGLE}
